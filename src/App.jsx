0b745d67a89c0a0a79634be4b71a739efccaf920
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-05 10:01:20 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
+import './App.css';
+
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 34,
+    photo: 'johndoe.jpg'
+};
 
 function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  )
+    return (
+        <div className="App">
+            <div className="person">
+                <img src="images/johndoe.jpg" />
+                <div className="data">
+                    <div className="name">
+                        {person.firstName} {person.lastName}
+                    </div>
+                    <div className="age">{person.age}</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
